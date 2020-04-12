ede1f378feb54f5419088e3427623287fbc6fbc0
shafay-azeem <62188504+shafay-azeem@users.noreply.github.com>
2020-04-12 15:47:38 +0500

    Add files via upload

---
+++ Startup code 1 for New York.py
@@ -0,0 +1,50 @@
+#CODE NO 1 FOR START UP PREDICTION OF NEW YORK :
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.compose import ColumnTransformer
+
+# Importing the dataset
+dataset = pd.read_csv('50_Startups.csv')
+X = dataset.iloc[:,:-1].values
+y = dataset.iloc[:,4].values
+
+# Splitting the dataset into the Training set and Test set
+"""from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)"""
+# Encoding categorical data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder= LabelEncoder()
+X[:, 3] = labelencoder.fit_transform(X[:, 3])
+onehotencoder = ColumnTransformer([("Geography", OneHotEncoder(), [3])], remainder = 'passthrough')
+X = onehotencoder.fit_transform(X)
+X = X[:, 0:1] 
+
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2, random_state=0)
+# Step 6 - Fit Regressor
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+a= regressor.predict(X_test)
+
+# Fitting Decision Tree Regression to the dataset
+from sklearn.tree import DecisionTreeRegressor
+regressor = DecisionTreeRegressor(random_state = 0)
+regressor.fit(X, y)
+
+pred = regressor.predict([[10000]])
+print('Profit=',pred)
+
+# Visualising the Decision Tree Regression results (higher resolution)
+X_grid = np.arange(min(X), max(X), 0.01)
+X_grid = X_grid.reshape((len(X_grid), 1))
+plt.scatter(X, y, color = 'red')
+plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
+plt.title('START UP OF NEW YORK(Decision Tree Regression)')
+plt.xlabel('New York startup')
+plt.ylabel('profit')
+plt.show()
